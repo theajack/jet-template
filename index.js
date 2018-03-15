fe28a4f07539a0a2d1703b35090132b9e387041e
theajack <1506282385@qq.com>
2018-03-15 16:51:36 +0800

    1

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 Jet.lang.use(['cn','en']);
 new Jet({
-  beforemount:function(){
-    this.p_top=(document.documentElement.clientHeight-555)/2;
-  },
-  data:{
-    p_top:95
-  },
-  func:{
-    
-  }
 })
 Jet.router.use({
   history:false,
