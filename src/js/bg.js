5a21c6398851d1d84463275ec90d8f1323b2d329
theajack <1506282385@qq.com>
2018-03-29 13:30:54 +0800

    a

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,123 +1,149 @@
 /**
- * Copyright (c) 2016 hustcc
- * License: MIT
- * Version: %%GULP_INJECT_VERSION%%
- * GitHub: https://github.com/hustcc/canvas-nest.js
+  author:theajack
+  www.theajack.com
 **/
-  //封装方法，压缩之后减少文件大小
-  var config_n=260;
-  function get_attribute(node, attr, default_value) {
-    return node.getAttribute(attr) || default_value;
-  }
-  //封装方法，压缩之后减少文件大小
-  function get_by_tagname(name) {
-    return document.getElementsByTagName(name);
-  }
-  //获取配置参数
-  function get_config_option() {
-    var scripts = get_by_tagname("script"),
-      script_len = scripts.length,
-      script = scripts[script_len - 1]; //当前加载的script
-    return {
-      l: script_len, //长度，用于生成id用
-      z: get_attribute(script, "zIndex", -1), //z-index
-      o: get_attribute(script, "opacity", 0.5), //opacity
-      c: get_attribute(script, "color", "0,0,0"), //color
-      n: config_n //count
+(function () {
+var canvas = $J.ct("canvas#bgCanvas");
+var _w=$J.width();
+var _h=$J.height();
+var _n=(_w<1000)?15:30;
+var ctx = canvas.getContext("2d");
+var circles=new Array();
+var pause=false;
+var frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(func) {
+        window.setTimeout(func, 1000 / 45);
     };
-  }
-  //设置canvas的高宽
-  function set_canvas_size() {
-    canvas_width = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, 
-    canvas_height = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-    initPage();
-  }
-  function initPage(){
-  }
-  //绘制过程
-  function draw_canvas() {
-    context.clearRect(0, 0, canvas_width, canvas_height);
-    //随机的线条和当前位置联合数组
-    var e, i, d, x_dist, y_dist, dist; //临时节点
-    //遍历处理每一个点
-    random_points.forEach(function(r, idx) {
-      r.x += r.xa, 
-      r.y += r.ya, //移动
-      r.xa *= r.x > canvas_width || r.x < 0 ? -1 : 1, 
-      r.ya *= r.y > canvas_height || r.y < 0 ? -1 : 1, //碰到边界，反向反弹
-      context.fillRect(r.x - 0.5, r.y - 0.5, 1, 1); //绘制一个宽高为1的点
-      //从下一个点开始
-      for (i = idx + 1; i < all_array.length; i++) {
-        e = all_array[i];
-        // 当前点存在
-        if (null !== e.x && null !== e.y) {
-          x_dist = r.x - e.x; //x轴距离 l
-          y_dist = r.y - e.y; //y轴距离 n
-          dist = x_dist * x_dist + y_dist * y_dist; //总距离, m
 
-          dist < e.max && (e === current_point && dist >= e.max / 2 && (r.x -= 0.03 * x_dist, r.y -= 0.03 * y_dist), //靠近的时候加速
-            d = (e.max - dist) / e.max,
-            context.beginPath(),
-            context.lineWidth = d ,
-            context.strokeStyle = "rgba(" + config.c + "," + (d + 0.2) + ")",
-            context.moveTo(r.x, r.y),
-            context.lineTo(e.x, e.y),
-            context.stroke());
-        }
-      }
-    }), frame_func(draw_canvas);
-  }
-  //创建画布，并添加到body中
-  
-  
-  var the_canvas = document.createElement("canvas"), //画布
-    config = get_config_option(),//配置
-    canvas_id = "c_n" + config.l, //canvas id
-    context = the_canvas.getContext("2d"), canvas_width, canvas_height, 
-    frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(func) {
-      window.setTimeout(func, 1000 / 45);
-    }, random = Math.random, 
-    current_point = {
-      x: null, //当前鼠标x
-      y: null, //当前鼠标y
-      max: 20000 // 圈半径的平方
-    },
-    all_array;
-    
-  the_canvas.id = canvas_id;
-  the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:" + config.z + ";opacity:" + config.o;
-  get_by_tagname("body")[0].appendChild(the_canvas);
+var cacheCanvas = document.createElement("canvas");
+var cache_ctx = cacheCanvas.getContext("2d");
 
-  //初始化画布大小
-  set_canvas_size();
-  window.onresize = set_canvas_size;
-  //当时鼠标位置存储，离开的时候，释放当前位置信息
-  window.onmousemove = function(e) {
-    e = e || window.event;
-    current_point.x = e.clientX;
-    current_point.y = e.clientY;
-  }, window.onmouseout = function() {
-    current_point.x = null;
-    current_point.y = null;
-  };
-  //随机生成config.n条线位置信息
-  for (var random_points = [], i = 0; config.n > i; i++) {
-    var x = random() * canvas_width, //随机位置
-      y = random() * canvas_height,
-      xa = 2 * random() - 1, //随机运动方向
-      ya = 2 * random() - 1;
-    // 随机点
-    random_points.push({
-      x: x,
-      y: y,
-      xa: xa,
-      ya: ya,
-      max: 6000 //沾附距离 
+(function () {
+    canvas.width=_w;
+    canvas.height=_h;
+    cacheCanvas.width = _w;
+    cacheCanvas.height = _h;
+    cacheCanvas.css({
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        'z-index': '-2'
     });
-  }
-  all_array = random_points.concat([current_point]);
-  //0.1秒后绘制
-  setTimeout(function() {
-    draw_canvas();
-  }, 100);
-  
+    var _cover = $J.ct("div#bgCover").css({
+        width:_w+"px",
+        height:_h+"px",
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        'z-index': '-2'
+    });
+    $J.body().prepend([canvas,_cover]);
+    for(var i=0;i<_n;i++){
+      circles.push(new Circle());
+    }
+    _draw();
+})();
+function _draw() {
+    if(!pause){
+        cache_ctx.clearRect(0, 0, _w, _h);
+        ctx.clearRect(0, 0, _w, _h);
+        circles.each(function (item) {
+            item.act();
+        });
+        ctx.drawImage(cacheCanvas, 0, 0, _w, _h);
+        frame_func(_draw);
+    }
+}
+function _getColorBase(){
+    return "rgba("+$J.random(0,255)+","+$J.random(0,255)+","+$J.random(0,255)+",";
+}
+function Circle(x,y) {
+    this.x=$J.checkArg(x,$J.random(0,_w));
+    this.y=$J.checkArg(y,$J.random(0,_h));
+    this.r=0;
+    this.max_r=$J.random(10,50);
+    this.speed=$J.random(2,6)*0.02;
+    this.alpha=$J.random(2,9)*0.1;
+    this.per_a=0;
+    //this.color_base=(($J.random(0,1)==0)?g_color:b_color);
+    this.color_base=_getColorBase();
+    this.color=this.color_base+this.alpha+")";
+    this.stop=false;
+    this.reinit=function(){
+        if(circles.length<=_n){
+            this.x=$J.random(0,_w);
+            this.y=$J.random(0,_h);
+            this.r=0;
+            this.max_r=$J.random(10,50);
+            this.speed=$J.random(2,6)*0.02;
+            this.alpha=$J.random(2,9)*0.1;
+            this.per_a=0;
+            //this.color_base=(($J.random(0,1)==0)?g_color:b_color);
+            this.color_base=_getColorBase();
+            this.color=this.color_base+this.alpha+")";
+        }else{
+            this.stop=true;
+            circles.remove(this);
+        }
+    };
+    this.act=function(){
+        if(!this.stop){
+            this.r += this.speed;
+            if (this.r > this.max_r * 0.7) {
+                if (this.r >= this.max_r) {
+                    this.reinit();
+                } else {
+                    if (this.alpha > 0) {
+                        if (this.per_a == 0) {
+                            this.per_a = (this.alpha) / ((this.max_r - this.r) / this.speed);
+                        }
+                        this.alpha -= this.per_a;
+                        if (this.alpha <= 0) {
+                            this.alpha=0;
+                        }
+                        this.color = this.color_base + this.alpha + ")";
+                    } else {
+                        this.alpha=0;
+                        this.color = this.color_base + this.alpha + ")";
+                    }
+                }
+            }
+            this.draw();
+        }
+    };
+    this.draw=function(){
+        cache_ctx.beginPath();
+        cache_ctx.arc(this.x , this.y ,this.r,0,2*Math.PI);
+        cache_ctx.fillStyle=this.color;
+        cache_ctx.fill();
+        cache_ctx.closePath();
+    };
+};
+window.onresize=function () {
+    _w=$J.width();
+    _h=$J.height();
+    cacheCanvas.width=_w;
+    cacheCanvas.height=_h;
+    canvas.width=_w;
+    canvas.height=_h;
+    $J.id("bgCover").css({
+        width:_w+"px",
+        height:_h+"px"
+    });
+}
+function _pause(){
+    if(pause){
+        pause=false;
+        _draw();
+    }else{
+        pause=true;
+    }
+}
+window.onclick=function (e) {
+    circles.push(new Circle(e.clientX,e.clientY));
+}
+window.onkeydown=function (e) {
+    if(e.keyCode===32){
+        _pause()
+    }
+}
+})();
